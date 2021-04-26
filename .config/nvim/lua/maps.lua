4d7a5ca5dd2c5fa2f0c91211a25681c4a2fb853b
hector8911 <mhhector6@gmail.com>
2021-04-26 01:12:55 -0600

    Drang and Drop

--- .config/nvim/lua/maps.lua
+++ .config/nvim/lua/maps.lua
@@ -25,12 +25,16 @@ C.map("i", "<Tab>", "v:lua.tab_confirm()", {expr = true})
 C.map("n", "<leader>f", ":bufdo normal gg=G<CR>", {noremap = true})
 
 -- LSP
-C.map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", {noremap = true, silent = true})
+C.map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opt)
 -- C.map("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>",{noremap = true, silent = true})
-C.map("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", {noremap = true, silent = true})
-C.map("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", {noremap = true, silent = true})
+C.map("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opt)
+C.map("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opt)
 
 C.map("t", "<esc>", [[<C-\><C-n>]], {noremap = true})
 
 C.map("n", "<leader>c", ":CommentToggle<CR>", opt)
 C.map("v", "<leader>c", ":CommentToggle<CR>", opt)
+
+C.map("n", "<leader>g", ":Neogit<CR>", opt)
+
+
